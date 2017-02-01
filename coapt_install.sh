f0f7353ab40863a2b43dd12d4f62971e3cd63c30
Tristan Chase <tristan.m.chase@gmail.com>
2017-02-01 13:52:05 -0500

    Tweaked install script

--- coapt_install.sh
+++ coapt_install.sh
@@ -24,10 +24,18 @@ dir=$HOME/tmp/sandbox
 
 ## Install missing $sys_deps
 echo "Installing system software needed for coapt to run..."
+echo ""
+sleep 2
 sudo aptitude install $sys_deps
-echo "Done."
+sleep 2
+echo "Done installing system software."
+echo ""
 
 ## Download raw $files from GitHub to $dir
+echo "Downloading script files from GitHub..."
+echo ""
+sleep 2
+
 mkdir -p $dir
 cd $dir
 
@@ -36,6 +44,10 @@ for file in $files; do
 	mv $file.sh $file
 	chmod 755 $file
 done
+
+sleep 2
+echo "Installation complete. You may now use coapt by typing it on the command line."
+echo ""
 
 ## Rename the $files (drop the .sh)
 
