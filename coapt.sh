21f705633462e0d30004c153ef9ed3e8f83434ec
Tristan Chase <tristan.m.chase@gmail.com>
2020-04-27 02:38:38 -0400

    Update coapt.sh
    * Upgrade first section with debug option
    * Rename functions and variables

--- coapt.sh
+++ coapt.sh
@@ -1,16 +1,28 @@
 #!/usr/bin/env bash
-set -euo pipefail
+
+# Low-tech debug mode
+if [[ "${1:-}" =~ (-d|--debug) ]]; then
+	set -x
+	_debug_file=""${HOME}"/tmp/$(basename "${0}")-debug.$$"
+	exec > >(tee "${_debug_file:-}") 2>&1
+	shift
+fi
+
+# Same as set -euE -o pipefail
+set -o errexit
+set -o nounset
 set -o errtrace
-#set -x
+set -o pipefail
 IFS=$'\n\t'
 
 #-----------------------------------
 
-#/ Usage: coapt [--help]
-#/ Description: A script meant to fit various apt-related scripts together.
-#/ Examples:
-#/ Options:
-#/   --help: Display this help message
+#//Usage: coapt [ {-d|--debug} ] [ {-h|--help} ]
+#//Description: A script meant to fit various apt-related scripts together.
+#//Examples: coapt; coapt --debug
+#//Options:
+#//	-d --debug	Enable debug mode
+#//	-h --help	Display this help message
 
 # Created: Long ago
 # Tristan M. Chase <tristan.m.chase@gmail.com>
@@ -22,66 +34,70 @@ IFS=$'\n\t'
 #-----------------------------------
 # Low-tech help option
 
-usage() { grep '^#/' "$0" | cut -c4- ; exit 0 ; }
-expr "$*" : ".*--help" > /dev/null && usage
+function __usage() { grep '^#/' "${0}" | cut -c4- ; exit 0 ; }
+expr "$*" : ".*-h\|--help" > /dev/null && __usage
 
 #-----------------------------------
 # Low-tech logging function
 
-readonly LOG_FILE=""$HOME"/tmp/$(basename "$0").log"
-info()    { echo "[INFO]    $*" | tee -a "$LOG_FILE" >&2 ; }
-warning() { echo "[WARNING] $*" | tee -a "$LOG_FILE" >&2 ; }
-error()   { echo "[ERROR]   $*" | tee -a "$LOG_FILE" >&2 ; }
-fatal()   { echo "[FATAL]   $*" | tee -a "$LOG_FILE" >&2 ; exit 1 ; }
+readonly LOG_FILE=""${HOME}"/tmp/$(basename "${0}").log"
+function __info()    { echo "[INFO]    $*" | tee -a "${LOG_FILE}" >&2 ; }
+function __warning() { echo "[WARNING] $*" | tee -a "${LOG_FILE}" >&2 ; }
+function __error()   { echo "[ERROR]   $*" | tee -a "${LOG_FILE}" >&2 ; }
+function __fatal()   { echo "[FATAL]   $*" | tee -a "${LOG_FILE}" >&2 ; exit 1 ; }
 
 #-----------------------------------
 # Trap functions
 
-traperr() {
-	info "ERROR: ${BASH_SOURCE[1]}.$$ at line ${BASH_LINENO[0]}"
+function __traperr() {
+	__error "${FUNCNAME[1]}: ${BASH_COMMAND}: $?: ${BASH_SOURCE[1]}.$$ at line ${BASH_LINENO[0]}"
 }
 
-ctrl_c(){
+function __ctrl_c(){
 	exit 2
 }
 
-cleanup() {
+function __cleanup() {
 	case "$?" in
 		0) # exit 0; success!
 			#do nothing
 			;;
 		2) # exit 2; user termination
-			info ""$(basename $0).$$": script terminated by user."
+			__info ""$(basename ${0}).$$": script terminated by user."
 			;;
 		3) # exit 3; reboot deferred
-			info ""$(basename $0).$$": reboot deferred by user."
+			__info ""$(basename ${0}).$$": reboot deferred by user."
 			;;
 		*) # any other exit number; indicates an error in the script
 			#clean up stray files
-			#fatal ""$(basename $0).$$": [error message here]"
+			#__fatal ""$(basename ${0}).$$": [error message here]"
 			;;
 	esac
+
+	if [[ -n "${_debug_file:-}" ]]; then
+		echo "Debug file is: "${_debug_file:-}""
+	fi
 }
 
 #-----------------------------------
 # Main script wrapper
 
-if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
-	trap traperr ERR
-	trap ctrl_c INT
-	trap cleanup EXIT
+if [[ "${BASH_SOURCE[0]}" = "${0}" ]]; then
+	trap __traperr ERR
+	trap __ctrl_c INT
+	trap __cleanup EXIT
 	#-----------------------------------
 	# Main Script
 
 	# Config files
-	config_dir="$HOME/.config/coapt"
-	held_packages_file="$config_dir/held-packages"
+	_config_dir="${HOME}/.config/coapt"
+	_held_packages_file="${_config_dir}/held-packages"
 
 	# Check for config files
-	if [[ ! -e $held_packages_file ]]; then
+	if [[ ! -e ${_held_packages_file} ]]; then
 		echo "Adding some missing config files..."
-		mkdir -p $config_dir
-		touch $held_packages_file
+		mkdir -p ${_config_dir}
+		touch ${_held_packages_file}
 	fi
 
 	# Create snapshot of installed packages (optional).  apt-snapshot is a separate script.
@@ -89,9 +105,9 @@ if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
 
 	# Autoremove packages? (May require reboot)
 	echo -n "Would you like to autoremove unused kernels and packages now? (May reqiure reboot) (y/N): "
-	read response
+	read _response
 
-	case $response in
+	case ${_response} in
 
 		y|Y)
 			sudo apt autoremove --purge
@@ -109,16 +125,18 @@ if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
 	# Update package lists.
 	sudo aptitude update
 
-	# Check to see if $HOME/.config/held-packages is empty
-	held_packages_empty=0 # 0=not empty, 1=empty
+	# TODO Clean up this section
+	# TODO Add option to change the list of held packages with help
+	# Check to see if ${HOME}/.config/held-packages is empty
+	_held_packages_empty=0 # 0=not empty, 1=empty
 
-	if [[ -s "$held_packages_file" ]]; then
+	if [[ -s "${_held_packages_file}" ]]; then
 		echo "The following packages will be held at their current version:"
-		held_packages="$(cat $held_packages_file)"
-		echo $held_packages
+		_held_packages="$(cat ${_held_packages_file})"
+		echo ${_held_packages}
 		echo ""
 	else
-		held_packages_empty=1
+		_held_packages_empty=1
 	fi
 
 	# If empty=yes=1, skip hold
@@ -126,16 +144,16 @@ if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
 	# Else hold
 
 	# Hold any packages at their current version?
-	if [ $held_packages_empty -ne 1 ]; then
-		sudo aptitude hold $held_packages
+	if [ ${_held_packages_empty} -ne 1 ]; then
+		sudo aptitude hold ${_held_packages}
 	fi
 
 	# Upgrade packages.
 	sudo aptitude upgrade
 
 	#Release hold on any held packages.
-	if [ $held_packages_empty -ne 1 ]; then
-		sudo aptitude unhold $held_packages
+	if [ ${_held_packages_empty} -ne 1 ]; then
+		sudo aptitude unhold ${_held_packages}
 	fi
 
 	# Clean package cache.
@@ -149,9 +167,9 @@ if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
 	if [ -f /var/run/reboot-required ]; then
 		cat /var/run/reboot-required
 		echo -n "Would you like to reboot the system now? (y/N): "
-		read response
+		read _response
 
-		case $response in
+		case ${_response} in
 			y|Y)
 				sudo reboot
 				;;
@@ -172,3 +190,7 @@ fi
 #-----------------------------------
 
 exit 0
+
+# TODO (bottom up)
+#
+# * Update dependencies section
