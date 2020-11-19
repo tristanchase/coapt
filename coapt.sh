a204be4e7fb283daa629c773bf83cacf81e630fe
Tristan Chase <tristan.m.chase@gmail.com>
2020-11-19 11:23:39 -0500

    Refactor hold packages and autoremove

--- coapt.sh
+++ coapt.sh
@@ -21,6 +21,9 @@
 # TODO Section
 #
 # * Update dependencies section
+# * Replace echo with printf
+# * Make autoremove an option
+# * Modularize steps with functions
 
 #-----------------------------------
 
@@ -33,36 +36,18 @@
 # Put main script here
 function __main_script {
 
-	## Config files
-	_config_dir="${HOME}/.config/coapt"
-	_held_packages_file="${_config_dir}/held-packages"
-
-	## Check for config files
-	if [[ ! -e ${_held_packages_file} ]]; then
-		echo "Adding some missing config files..."
-		mkdir -p ${_config_dir}
-		touch ${_held_packages_file}
-		__info__ "Added file "${_held_packages_file}"."
-	fi
-
 	## Create snapshot of installed packages (optional).  apt-snapshot is a separate script.
 	#apt-snapshot create
 
 	## Autoremove packages? (May require reboot)
-	echo -n "Would you like to autoremove unused kernels and packages now? (May reqiure reboot) (y/N): "
-	read _response
+	echo -n "Autoremove unused kernels and packages now? (May reqiure reboot) (y/N): "
+	read _autoremove_yN
+	function __autoremove__ {
+		__lock_check
+		sudo apt autoremove --purge
+	}
+	[[ "${_autoremove_yN}" =~ (y|Y) ]] && __autoremove__ || printf "%b\n" "Autoremove: skipped"
 
-	case ${_response} in
-
-		y|Y)
-			__lock_check
-			sudo apt autoremove --purge
-			;;
-
-		*)
-			#do nothing
-			;;
-	esac
 
 	echo ""
 	echo "Updating..."
@@ -76,28 +61,13 @@ function __main_script {
 	mkdir -p "${_hold_dir}"
 	_held_packages=( $(basename $(printf "%b\n" "${_hold_dir}"/*) ) )
 
-	# TODO Clean up this section; get rid of _held_packages_empty variable; base tests on _held_packages_file
-	# TODO Add option to change the list of held packages with help
-	# Check to see if ${HOME}/.config/held-packages is empty
-	_held_packages_empty=0 # 0=not empty, 1=empty
 
-	if [[ -s "${_held_packages_file}" ]]; then
+	if [[ -n "${_held_packages}" ]]; then
 		echo "The following packages will be held at their current version:"
-		#_held_packages="$(cat ${_held_packages_file})"
 		aptitude versions $(printf "%b\n" "${_held_packages[@]}")
 		echo ""
-	else
-		_held_packages_empty=1
-	fi
-
-	# If empty=yes=1, skip hold
-
-	# Else hold
-
-	# Hold any packages at their current version?
-	if [ ${_held_packages_empty} -ne 1 ]; then
 		__lock_check
-		sudo aptitude hold ${_held_packages}
+		sudo aptitude -q=3 hold ${_held_packages}
 	fi
 
 	## Upgrade packages.
@@ -133,10 +103,9 @@ function __main_script {
 } #end __main_script
 
 # Local functions
-
 function __local_cleanup {
 	## Release hold on any held packages.
-	if [ ${_held_packages_empty} -ne 1 ]; then
+	if [[ -n "${_held_packages}" ]]; then
 		printf "%b\n"
 		printf "%b" "Releasing hold on packages..."
 		__lock_check
